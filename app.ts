529b898fc6f0cc9d6526affd41022fe4c2111a04
equix1 <jeremiegauthier6@gmail.com>
2025-09-20 14:03:21 -0400

    Prettier sweep

--- app.ts
+++ app.ts
@@ -1,6 +1,6 @@
-import express from 'express';
-import { router } from './src/router';
-import './src/utils/expandEnv'; // Uses dotenv to expand .env
+import express from "express";
+import { router } from "./src/router";
+import "./src/utils/expandEnv"; // Uses dotenv to expand .env
 
 const app = express();
 
